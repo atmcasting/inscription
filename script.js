5600f8a0978759e55ac1b4f279d51d6032c8bbe3
atmcasting <atm.casting.contact@gmail.com>
2024-06-04 20:13:30 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+if (Math.random() > 0.5) {
+  window.location.href = "https://4b5c75fa-28cf-4f54-b951-fbea32422106-00-3df6rrqg9t28w.picard.replit.dev/";
+} else {
+  window.location.href = "https://4b5c75fa-28cf-4f54-b951-fbea32422106-00-3df6rrqg9t28w.picard.replit.dev/";
+}
